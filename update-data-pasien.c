3d9c8dd042cfea4915dc8cd6a70a9f5ca115b1a0
Pradigta Hisyam Ramadhan <pradigta.phr@gmail.com>
2024-06-13 19:06:57 +0700

    update: Finalisasi bagian digta

--- update-data-pasien.c
+++ update-data-pasien.c
@@ -372,6 +372,21 @@ void start_program(Pasien **head) {
             break;
         }
 
+        case 2:
+        
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
         default:
             printf("Input tidak valid! Silakan masukkan angka dari 1-6!\n");
             break;
