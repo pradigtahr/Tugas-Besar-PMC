893cd69105a4ae3aa437466506c75a47749c7f38
Kanzhaha <161725015+Kanzhaha@users.noreply.github.com>
2024-06-17 16:23:05 +0700

    Create GUI_page.h

---
+++ GUI_page.h
@@ -0,0 +1,65 @@
+#ifndef PAGE1_H
+#define PAGE1_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page1(void);
+
+#endif /* PAGE1_H */
+
+//__________________________________//
+
+#ifndef PAGE2_H
+#define PAGE2_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page2(void);
+
+#endif /* PAGE2_H */
+
+//__________________________________//
+
+#ifndef PAGE3_H
+#define PAGE3_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page3(void);
+
+#endif /* PAGE3_H */
+
+//__________________________________//
+
+#ifndef PAGE4_H
+#define PAGE4_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page4(void);
+
+#endif /* PAGE4_H */
+
+//__________________________________//
+
+#ifndef PAGE5_H
+#define PAGE5_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page5(void);
+
+#endif /* PAGE5_H */
+
+//__________________________________//
+
+#ifndef PAGE6_H
+#define PAGE6_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *create_page6(void);
+
+#endif /* PAGE6_H */
+
+
